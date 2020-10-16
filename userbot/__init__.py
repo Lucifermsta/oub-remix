f0038dfa7c6cf0fe72df9c81c12bad7cc7302813
jkthe1 <70848090+jkthe1@users.noreply.github.com>
2020-10-16 16:40:56 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -91,9 +91,9 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/sahyam2019/oub-remix.git")
+    "https://github.com/jkthe1/oub-remix.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "sql-extended")    
+    "UPSTREAM_REPO_BRANCH", "jkthe1")    
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
